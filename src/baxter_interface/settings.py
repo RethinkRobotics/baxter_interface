b4b04d427f0477c9d94d3534a941dbe237139068
Ian McMahon <imcmahon@rethinkrobotics.com>
2015-10-29 16:25:08 -0400

    Version Bumped SDK to 1.2

--- src/baxter_interface/settings.py
+++ src/baxter_interface/settings.py
@@ -29,13 +29,13 @@ JOINT_ANGLE_TOLERANCE = 0.008726646
 HEAD_PAN_ANGLE_TOLERANCE = 0.1396263401
 
 ## Versioning
-SDK_VERSION = '1.1.1'
+SDK_VERSION = '1.2.0'
 CHECK_VERSION = True
 # Version Compatibility Maps - {current: compatible}
-VERSIONS_SDK2ROBOT = {'1.1.1': ['1.1.1', '1.1.0']}
-VERSIONS_SDK2GRIPPER = {'1.1.1':
+VERSIONS_SDK2ROBOT = {'1.2.0': ['1.2.0']}
+VERSIONS_SDK2GRIPPER = {'1.2.0':
                           {
                            'warn': '2014/5/20 00:00:00',  # Version 1.0.0
                            'fail': '2013/10/15 00:00:00', # Version 0.6.2
                           }
-                         }
+                       }
